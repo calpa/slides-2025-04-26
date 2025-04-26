e68c829434c51cc5b8ae2988a72d5d58a8b3b943
Calpa Liu <calpaliu@gmail.com>
2025-04-26 13:49:09 +0800

    Add QR codes and links to footer for interactive notes and slides

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,6 +1,30 @@
 <!-- global-bottom.vue -->
 <template>
-    <footer class="absolute w-full flex bottom-0 items-end justify-end z-[9999]">
+    <footer class="absolute w-full flex bottom-0 items-end justify-between z-[9999]">
+        <div class="flex items-center flex-row">
+            <div v-for="item in links" :key="item.text" class="text-center">
+                <div class="p-2 text-slate-400 text-sm">{{ item.text }}</div>
+                <img :src="item.qrcodeImage" class="w-18 h-18 p-2" alt="QR Code" />
+            </div>
+        </div>
         <div class="p-2 text-slate-400 text-sm">Calpa Liu</div>
     </footer>
 </template>
+
+<script setup>
+import { useNav } from '@slidev/client'
+const { currentPage } = useNav()
+
+const qrcodeBase = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&bgcolor=e2e8f0`
+
+const links = [
+    {
+        text: '互動筆記',
+        qrcodeImage: `${qrcodeBase}&data=https://pad.riseup.net/p/Wz657VgFwWQKEcIjhhT4`
+    },
+    {
+        text: '簡報',
+        qrcodeImage: `${qrcodeBase}&data=https://slides-bolt-new.calpa.me/`
+    }
+]
+</script>
